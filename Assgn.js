0b9cb379410736e2d2365627f5dee9bee5e2720e
student <joycemumbiiwanjiku@gmail.com>
2023-05-11 14:22:37 +0300

    b

--- Assgn.js
+++ Assgn.js
@@ -2,20 +2,20 @@
 //  delay time in milliseconds. The function should log the message to the console 
 //  after the specified delay time.
 
-
-async function HelloWorld( HelloWorld, delay) {
-  await new Promise(resolve => setTimeout(resolve, delay));
-  console.log(HelloWorld);
+async function HelloWorld(Hello,World,delay){
+await new Promise((resolve, reject) =>setTimeout(resolve,delay)) 
 }
-console.log("Hello, world!", 1000);
+console.log("Hello,World!",1000);
+
+ 
+  
 // You have an array of user IDs and a function getUserData(id)
 //  that returns a Promise with user data when given a user ID. Write 
 //  an asynchronous function that fetches and logs the data for each user
 //   ID one by one, in sequence.
-
-async function fetchAndUserData(userIds) {
-  for (let i = 0; i < userIds.length; i++) {
-    const userData = await getUserData(userIds[i]);
+async function fetchAndUserData(userID){
+  for(let i=0; i < userID.length;i++){
+    const userData=await getUserData(userid[i]);
     console.log(userData);
   }
 }
@@ -27,11 +27,11 @@ console.log(fetchAndUserData);
 //   Write a function that calls performTask() and logs a custom success message if 
 //   the task is successful, and
 //  a custom error message if there's an error.
-async function runTask() {
-  try {
-    await performTask();
-    console.log('Task completed successfully!');
-  } catch (error) {
-    console.error('Error while performing task:', error);
+async function runTask(){
+  try{
+     await performaTask();
+     console.log('Task completed successfully!');
+  }catch(error){
+    console.log('Erro while performing task:',error);
   }
 }
